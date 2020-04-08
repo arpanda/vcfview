674b1f419847845d81525e9338c870040a152004
arpanda <arijpanda@gmail.com>
2020-04-07 19:00:09 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+define([
+    'dojo/_base/declare',
+    'JBrowse/Plugin'
+],
+function( declare, JBrowsePlugin) {
+
+return declare( JBrowsePlugin, {
+    constructor: function( args ) {
+    var browser = this.browser;
+
+    /* do anything you need to initialize your plugin here */
+    }
+});
+
+});
