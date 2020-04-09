04534f9d90921d60f78afc52dcdb6e7d7d95484a
arpanda <arijpanda@gmail.com>
2020-04-09 13:11:20 -0500

    -

--- js/store/SeqFeature/vcf_view.js
+++ js/store/SeqFeature/vcf_view.js
@@ -4,15 +4,21 @@ define([
     'dojo/_base/array',
     'dojo/promise/all',
     'JBrowse/Store/SeqFeature',
-    'JBrowse/Store/DeferredStatsMixin',
-    'JBrowse/Store/DeferredFeaturesMixin',
-], function (declare, long, array, all, SeqFeatureStore, DeferredFeaturesMixin, DeferredStatsMixin) {
+], function (
+    declare, 
+    long, 
+    array, 
+    all, 
+    SeqFeatureStore,
+    ) {
 
-    return declare([SeqFeatureStore, DeferredFeaturesMixin, DeferredStatsMixin ], {
+    return declare([SeqFeatureStore ], {
         constructor: function (args) {
             console.log('tesing in');
             /* do anything you need to initialize your plugin here */
         }
+
+
     });
 
 });
