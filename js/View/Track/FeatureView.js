506dd43fa415dd438caf04a9ac215a771eb4b411
arpanda <arijpanda@gmail.com>
2020-04-10 09:16:42 -0500

    rename the project

--- js/View/Track/FeatureView.js
+++ js/View/Track/FeatureView.js
@@ -2,12 +2,12 @@ define([
     'dojo/_base/declare',
     'JBrowse/View/Track/FeatureView',
 ],
-function(
+function (
     declare,
     FeatureView
 ) {
     return declare(FeatureView, {
-        renderDetailValue: function(parent, title, val, f, class_) {
+        renderDetailValue: function (parent, title, val, f, class_) {
             if(!val) return;
             this.inherited(arguments);
         }
