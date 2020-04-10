1365ba1145f3d7c95902c55c198fdd03313fa074
arpanda <arijpanda@gmail.com>
2020-04-10 13:29:28 -0500

    -

--- js/Store/SeqFeature/vcfview.js
+++ js/Store/SeqFeature/vcfview.js
@@ -18,12 +18,21 @@ define([
             this.chunkSize = args.chunkSize || 10000
             this.urlTemplate = args.urlTemplate
             this.baseUrl = args.baseUrl
+            this.type = args.type
 
             console.log(this.urlTemplate)
             console.log(this.chunkSize)
+            console.log(this.type)
 
             //console.log(args.config);
             /* do anything you need to initialize your plugin here */
+        }
+
+        getFeatures(query, featureCallback, finishCallback, errorCallback) {
+            query.toString = () => `${query.ref},${query.start},${query.end}`
+            const s = query.start - query.start % chunkSize
+            const e = query.end + (chunkSize - query.end % chunkSize)
+
         }
 
     });
