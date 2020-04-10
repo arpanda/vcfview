1b6a6b53cbeb5fdd48aef00b65c0c31391938c58
arpanda <arijpanda@gmail.com>
2020-04-10 11:27:49 -0500

    -

--- js/Store/SeqFeature/vcfview.js
+++ js/Store/SeqFeature/vcfview.js
@@ -2,13 +2,11 @@ define([
     'dojo/_base/declare',
     'dojo/_base/lang',
     'dojo/_base/array',
-    'dojo/promise/all',
     'JBrowse/Store/SeqFeature',
 ], function (
     declare, 
     long, 
     array,
-    all,
     SeqFeatureStore,
     ) {
 
