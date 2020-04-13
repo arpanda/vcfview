945a522e8e2aff6c2cbcedf86c0aa915eb1d6d12
arpanda <arijpanda@gmail.com>
2020-04-13 16:30:12 -0500

    -

--- js/Store/SeqFeature/vcfview.js
+++ js/Store/SeqFeature/vcfview.js
@@ -1,25 +1,21 @@
 
 define([
     'dojo/_base/declare',
-    'dojo/_base/lang',
-    'dojo/_base/array',
-    'dojo/promise/all',
-    'JBrowse/Store/SeqFeature'
-], function(declare, long, array, all) {
+    'dojo/_base/lang'
+], function(declare, long) {
 
-    return declare([SeqFeatureStore], {
-        constructor: function (args) {
+    return declare(null, {
+        constructor: function(args) {
             console.log("testing the folder name");
+            /*
             this.chunkSize = args.chunkSize || 10000
             this.urlTemplate = args.urlTemplate
             this.baseUrl = args.baseUrl
 
-
             console.log(this.urlTemplate);
             console.log(this.chunkSize);
 
-            //console.log(args.config);
-            /* do anything you need to initialize your plugin here */
+            */
         }
 
     });
