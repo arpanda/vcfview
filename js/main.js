623472b62fb9880a1a9079b63825c1280269469d
arpanda <arijpanda@gmail.com>
2020-04-13 12:41:32 -0500

    -

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ define([
     'dojo/_base/declare',
     'JBrowse/Plugin'
 ],
-function ( declare, JBrowsePlugin) {
+function (declare, JBrowsePlugin) {
 
     return declare( JBrowsePlugin, {
         constructor: function ( /* args */ ) {
-            console.log('vcfview plugin starting')
+            console.log('vcfview plugin starting');
         }
     });
 
