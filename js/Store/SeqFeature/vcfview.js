3734c4eb34e1fe89ddc9daae271a54abc8ee8ad3
arpanda <arijpanda@gmail.com>
2020-04-14 15:13:10 -0500

    -

--- js/Store/SeqFeature/vcfview.js
+++ js/Store/SeqFeature/vcfview.js
@@ -6,7 +6,7 @@ define([
         return declare(VCFTabix, {
             getFeatures: function (query, featureCallback, finishCallback, errorCallback) {
                 this.inherited(arguments, [query,  (feature) => {
-                    console.log(feature.get('genotypes'))
+                    console.log(feature.get('genotypes').Sample.DP[0])
                     featureCallback(feature)
 
                 }, finishCallback, errorCallback])
