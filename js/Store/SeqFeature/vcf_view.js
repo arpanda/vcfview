1c5c3a622c9f97286ced43d0de2d10bbd93cf6a7
arpanda <arijpanda@gmail.com>
2020-04-13 20:22:36 -0500

    -

---
+++ js/Store/SeqFeature/vcf_view.js
@@ -0,0 +1,10 @@
+define(
+    ['dojo/_base/declare', 'dojo/_base/lang'],
+    function (declare, lang) {
+        return declare(null, {
+           constructor: function (args) {
+               console.log("I am inside vcf view ");
+           }
+        });
+    }
+);
