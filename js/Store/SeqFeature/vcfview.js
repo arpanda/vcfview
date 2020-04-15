e7f23c7127871ef64b194354f17681580806f1c7
arpanda <arijpanda@gmail.com>
2020-04-14 20:16:21 -0500

    -

--- js/Store/SeqFeature/vcfview.js
+++ js/Store/SeqFeature/vcfview.js
@@ -9,10 +9,10 @@ define([
                 this.inherited(arguments, [query,  (feature) => {
                     console.log(feature)
                     //console.log(feature.get('genotypes').Sample.DP.values[0])
-                    const sample_info = feature.get('genotypes').TCGA-FG-6691-01A-11D-1891.DP.values[0];
+                    var sample_info = feature.get('genotypes').TCGA-FG-6691-01A-11D-1891.DP.values[0];
                     console.log(sample_info)
 
-                    var sample_data = new SimpleFeature({ id: feature.get('id'), data: { start:feature.get('start'), end:feature.get('end'), score: feature.get('genotypes').Sample.DP.values[0] }})
+                    var sample_data = new SimpleFeature({ id: feature.get('id'), data: { start:feature.get('start'), end:feature.get('end'), score: sample_info }})
                     featureCallback(sample_data)
 
                 }, finishCallback, errorCallback])
