e1061a5a489c1d17e50312974433dd8c8831adac
Colin <colin.diesh@gmail.com>
2020-08-04 23:30:05 -0400

    MultiBigWig raw tabix

--- js/Store/SeqFeature/VCFRawTabix.js
+++ js/Store/SeqFeature/VCFRawTabix.js
@@ -18,6 +18,8 @@ define([
     },
     async _readChunk(query) {
       const parser = await this.getParser();
+      const samples = parser.samples;
+
       const regularizedReferenceName = this.browser.regularizeReferenceName(
         query.ref,
       );
@@ -26,9 +28,11 @@ define([
       let binSize = 100000;
       var bins = [];
       for (let i = 0; i < end; i += binSize) {
-        bins.push({ score: 0, count: 0 });
+        bins.push({
+          samples: samples.map(() => ({ score: 0, count: 0 })),
+        });
       }
-      console.time('vcfraw');
+
       await this.indexedData.getLines(
         regularizedReferenceName,
         0,
@@ -36,14 +40,17 @@ define([
         (line, fileOffset) => {
           const fields = line.split('\t');
           const start = +fields[1];
-          const score = +fields[9].split(':')[2];
-
           const featureBin = Math.max(Math.floor(start / binSize), 0);
-          bins[featureBin].score += isNaN(score) ? 0 : score;
-          bins[featureBin].count++;
           bins[featureBin].start = featureBin * binSize;
           bins[featureBin].end = (featureBin + 1) * binSize;
           bins[featureBin].id = fileOffset;
+          for (let i = 0; i < samples.length; i++) {
+            const sampleName = samples[i];
+            const score = +fields[9 + i].split(':')[2];
+            bins[featureBin].samples[i].score += isNaN(score) ? 0 : score;
+            bins[featureBin].samples[i].count++;
+            bins[featureBin].samples[i].source = sampleName;
+          }
         },
       );
       console.timeEnd('vcfraw');
@@ -60,13 +67,16 @@ define([
         const features = await this.featureCache.get(query.ref, query);
         features.forEach(feature => {
           if (feature.end > query.start && feature.start < query.end) {
-            featureCallback(
-              new SimpleFeature({
-                data: Object.assign(Object.create(feature), {
-                  score: feature.score / feature.count,
+            feature.samples.forEach(sample => {
+              featureCallback(
+                new SimpleFeature({
+                  data: Object.assign(Object.create(feature), {
+                    score: sample.score / sample.count,
+                    source: sample.source,
+                  }),
                 }),
-              }),
-            );
+              );
+            });
           }
         });
 
